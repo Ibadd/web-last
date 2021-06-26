c7b00a49f35b6f4db061952e41081cc297ceabd6
Ibad <muhammaditsfan@gmail.com>
2021-06-26 23:46:09 +0700

    Update layout.blade.php

--- resources/views/admin/template/layout.blade.php
+++ resources/views/admin/template/layout.blade.php
@@ -4,8 +4,10 @@
 <head>
 
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
     <title>@yield('title')</title>
 
