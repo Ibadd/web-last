d6f2b92a8eb82c278ad373b9cff764321f8c34b6
Ibad <muhammaditsfan@gmail.com>
2021-06-26 23:12:19 +0700

    icon

--- resources/views/admin/template/sidebar.blade.php
+++ resources/views/admin/template/sidebar.blade.php
@@ -10,7 +10,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin">
                 <div class="sidebar-brand-icon">
-                    <i class="fab fa-user-secret"></i>
+                    <i class="fas fa-user-secret"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Admin</div>
             </a>
